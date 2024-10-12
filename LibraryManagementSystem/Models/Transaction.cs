8c625fc5e10eb4a210f541f6200168fbf958a1b5
Helaly1804 <elhelaly1804@gmail.com>
2024-10-12 16:28:36 +0300

    adding transaction

---
+++ LibraryManagementSystem/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagementSystem.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int Total_Cost { get; set; }
+        public DateOnly CheckOut_Date { get; set; }
+        public DateOnly Return_Date { get; set; }
+        public DateOnly Due_Date {  get; set; }
+        public DateOnly OverDueDateDays;
+
+
+
+    }
+}
